382815bc052f51f5cddcca428cd4d5a7ffc3430a
Maya <maya.atiah.99@outlook.com>
2023-08-14 11:40:23 +0300

    navbar

--- src/Components/NavbarMobile.jsx
+++ src/Components/NavbarMobile.jsx
@@ -3,7 +3,7 @@ import "./NavbarMobile.css";
 import { FaAlignRight} from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+
 
 const NavbarMobile = (props) => {
   const [open, setOpen] = useState(false);
@@ -17,70 +17,35 @@ const NavbarMobile = (props) => {
 
   const closeMobileMenu = () => setOpen(false);
 
-  const animatedForm = { opacity: 0, y: -30 };
-  const animatedTo = { opacity: 1, y: 0 };
+ 
   return (
     <div>
       {open ? closeIcon : hamburgerIcon}
-      <div className='navbar-mobile-container'>
-        {open && (
-          <div className='navlink-mobile'>
-            <div className='one'>
-              <Link  style={{ textDecoration: "none" }}>
-                <div onClick={() => closeMobileMenu()}>
-                  <motion.div
-                    initial={animatedForm}
-                    animate={animatedTo}
-                    className='navbar-divstyle'
-                  >
-                    Home
-                  </motion.div>
-                </div>
-              </Link>
-            </div>
-
-            <div className='one'>
-              <Link  style={{ textDecoration: "none" }}>
-                <div onClick={() => closeMobileMenu()}>
-                  <motion.div
-                    initial={animatedForm}
-                    animate={animatedTo}
-                    className='navbar-divstyle'
-                  >
-                    Feed
-                  </motion.div>
-                </div>
-              </Link>
-            </div>
-
-            <div className='one'>
-              <Link  style={{ textDecoration: "none" }}>
-                <div onClick={() => closeMobileMenu()}>
-                  <motion.div
-                    initial={animatedForm}
-                    animate={animatedTo}
-                    className='navbar-divstyle'
-                  >
-                    About Us
-                  </motion.div>
-                </div>
-              </Link>
-            </div>
-            <div className='one'>
-              <Link  style={{ textDecoration: "none" }}>
-                <div onClick={() => closeMobileMenu()}>
-                  <motion.div
-                    initial={animatedForm}
-                    animate={animatedTo}
-                    className='navbar-divstyle'
-                  >
-                   Inquire Now
-                  </motion.div>
-                </div>
-              </Link>
-            </div>
+      <div className={`navbar-mobile-container ${open ? "open" : ""}`}>
+        <div className='navlink-mobile'>
+          <div className='one'>
+            <Link style={{ textDecoration: "none" }}>
+              <div onClick={() => closeMobileMenu()}>Home</div>
+            </Link>
           </div>
-        )}
+
+          <div className='one'>
+            <Link style={{ textDecoration: "none" }}>
+              <div onClick={() => closeMobileMenu()}>Feed</div>
+            </Link>
+          </div>
+
+          <div className='one'>
+            <Link style={{ textDecoration: "none" }}>
+              <div onClick={() => closeMobileMenu()}>About Us</div>
+            </Link>
+          </div>
+          <div className='one'>
+            <Link style={{ textDecoration: "none" }}>
+              <div onClick={() => closeMobileMenu()}>Inquire Now</div>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
